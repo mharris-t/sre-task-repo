a8059a182aa8edf6c1af51b0d4b6b57e60e80e21
Munavar Harris Thottoli <60294955+mharris-t@users.noreply.github.com>
2024-04-07 10:12:33 +0000

    adding init script

---
+++ init_env.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo 'Installing Prometheus....'
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+helm install prometheus prometheus-community/prometheus -f prometheus.yml --namespace sre
+
+echo 'Installing Grafana....' 
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+helm install grafana grafana/grafana --namespace sre --set adminPassword="admin"
+
+echo 'Deploying application...'
+kubectl apply -f deployment.yml -n sre
+kubectl apply -f service.yml -n sre
+
+echo 'Checking service...'
+minikube service upcommerce-service -n sre
